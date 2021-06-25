5df90a3c01c7f9cd4ad8f78e30f211aaa6307efe
ishan <ishan365.ia@gmail.com>
2021-06-25 18:52:49 +0530

    add moody line options

--- colors/modus-operandi.lua
+++ colors/modus-operandi.lua
@@ -3,3 +3,7 @@ vim.o.bg = 'light'
 local colors = require('modus-themes.modus_operandi').setup()
 require('modus-themes.highlights').core_highlights(colors)
 require('modus-themes.highlights').set_terminal(colors)
+
+if vim.g.modus_moody_enable == 1 then
+	require('modus-themes.galaxyline').set_statusline(colors)
+end
--- colors/modus-vivendi.lua
+++ colors/modus-vivendi.lua
@@ -4,3 +4,7 @@ local colors = require('modus-themes.modus_vivendi').setup()
 require('modus-themes.highlights').core_highlights(colors)
 require('modus-themes.highlights').set_terminal(colors)
 
+if vim.g.modus_moody_enable == 1 then
+	require('modus-themes.galaxyline').set_statusline(colors)
+end
+
